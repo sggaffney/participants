6d0e4c8c87677721befb8e0867e1e18afb67ee8e
Stephen Gaffney <stephen.gaffney@yale.edu>
2022-06-19 20:29:53 +0100

    Set logging level when creating app.

--- src/participants/app/__init__.py
+++ src/participants/app/__init__.py
@@ -10,7 +10,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
-
+    app.logger.setLevel(app.config["LOGGING_LEVEL"])
     db.init_app(app)
 
     from .api import api as api_blueprint
